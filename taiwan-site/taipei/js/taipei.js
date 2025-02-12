6acab9fd6ce46e08434377c5a70a34f8977430b4
yuhirata3018 <rocksteady3018@gmail.com>
2025-02-12 14:09:45 +0900

    modalが同じページ開いてまう

--- taiwan-site/taipei/js/taipei.js
+++ taiwan-site/taipei/js/taipei.js
@@ -6,3 +6,24 @@ function openModal() {
 function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
+
+function openModal() {
+    document.getElementById("modal2").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("modal2").style.display = "none";
+}
+
+function openModal() {
+    document.getElementById("modal3").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("modal3").style.display = "none";
+}
+
+function openModal() {
+    document.getElementById("modal4").style.display = "flex";
+}
+function closeModal() {
+    document.getElementById("modal4").style.display = "none";
+}
