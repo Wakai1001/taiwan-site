91874cc02b2979bc7f35afa8cb89dc839cb3bef7
mkpq <mmmpq.pro@gmail.com>
2025-02-17 13:51:18 +0900

    jsdekita

--- taiwan-site/top/js/top.js
+++ taiwan-site/top/js/top.js
@@ -1,9 +1,32 @@
 'use strict';
 
-const btn = document.querySelectorAll('.btn');
+img1.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img1.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img2.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img3.jpg';
+}
 
-btn.forEach(function (item, index) {
-  item.onclick = function () {
-    document.getElementById('photo').src = this.dataset.image;
-  }
-});
+img2.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img2.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img3.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img4.jpg';
+}
+
+img3.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img3.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img4.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img5.jpg';
+}
+
+img4.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img4.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img5.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img1.jpg';
+}
+
+img5.onclick = function () {
+  document.getElementById('photo').src = 'top/image/top_gallery_img5.jpg';
+  document.getElementById('photo2').src = 'top/image/top_gallery_img1.jpg';
+  document.getElementById('photo3').src = 'top/image/top_gallery_img2.jpg';
+}
+
